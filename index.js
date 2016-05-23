72674525a993d60be0fbf8566ecc55a324281c0c
kickinbahk <kickinbahk@gmail.com>
2016-05-23 07:55:53 -0700

    fix the port

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'root'
+  password: 'root',
+  database: 'test',
+  port: '3306',
 })
+
+connection.connect()
 
 connection.query(
   'SELECT "foo" AS first_field, "bar" AS second_field',
